ca1dd9d96f639e651f0561d1220074c8a8f9eea9
Patrik <patrik.tipek@email.cz>
2019-11-15 00:13:50 +0100

    add new functions

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function start() {
         return;
     }
 
-    const texture = loadTexture(gl, './texture/texture.jpg');
+    //const texture = loadTexture(gl, './texture/texture.jpg');
 
     const pr = new Program();
 
@@ -29,10 +29,18 @@ function start() {
     const program = pr.createProgram(gl, vertexShader, fragmentShader);
 
     //
+    // texture
+    //
+    let image = new Image();
+    image = loadTexture(gl, image, './texture/david.png');
+
+    //
     // attributes
     //
     const positionLocation = gl.getAttribLocation(program, 'inPosition');
-    var textureLocation = gl.getUniformLocation(program, "tex");
+    const textureLocation = gl.getUniformLocation(program, 'tex');
+    const resolution = gl.getUniformLocation(program, "uMatrix");
+    const programMode = gl.getUniformLocation(program, "mode");
 
     //
     // Create a vertex array object
@@ -49,12 +57,12 @@ function start() {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     const positions = [
-        0, 0,
-        0, 1,
-        1, 0,
-        1, 0,
-        0, 1,
-        1, 1,
+        0.0,  0.0,
+        1.0,  0.0,
+        0.0,  1.0,
+        0.0,  1.0,
+        1.0,  0.0,
+        1.0,  1.0,
     ];
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
@@ -70,8 +78,36 @@ function start() {
     //
     // variables
     //
-    let move = [0, 0, -1];
-    let speed = 1;
+    const picture = document.getElementById('picture');    
+    const file = document.getElementById('file');
+    const dropdown = document.getElementById('mode');
+    let src = picture.value;
+    let mode = parseInt(dropdown.value);
+
+    file.onchange = () => {
+        src = file.value;
+        console.log(src);
+        loadTexture(gl, image, src)
+    }
+
+    picture.onchange = () => {
+        src = picture.value;
+        console.log(src);
+        loadTexture(gl, image, src)
+    }
+
+    dropdown.onchange = () => {
+        mode = parseInt(dropdown.value);
+    }
+    
+    var dstWidth = image.width;
+    var dstHeight = image.height;
+
+    // convert dst pixel coords to clipspace coords      
+    var clipX = gl.canvas.width  *  2 - 1;
+    var clipY = gl.canvas.height * -2 + 1;
+    var clipWidth = dstWidth  / gl.canvas.width  *  2;
+    var clipHeight = dstHeight / gl.canvas.height * -2;
 
     //
     // draw scene
@@ -83,36 +119,19 @@ function start() {
     //
     window.onkeyup = (e) => {
         if (e.keyCode == 32) { // space
-            move[1] -= 0.3;
         }
         if (e.keyCode == 68) { // d
-            //translation[1] += 5;
-            move[0] -= 0.2;
-            console.log(move);
         }
         if (e.keyCode == 65) { // a
-            move[0] += 0.2;
-            console.log(move);
         }
         if (e.keyCode == 87) { // w
-            move[2] += 0.2;
-            console.log(move);
         }        
         if (e.keyCode == 83) { // s
-            move[2] -= 0.2;
-            console.log(move);
         }
         if (e.keyCode == 88) { // x
-            move[1] += 0.3;
-            console.log(move);
         }
         draw();
-    }
-
-    //
-    // camera
-    //
-    
+    }    
 
     function draw() {
         // current size of screen
@@ -122,80 +141,63 @@ function start() {
         gl.clearColor(1.0, 1.0, 1.0, 1.0); // rgba
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        // z-buffer
-        gl.enable(gl.DEPTH_TEST);
-
-        //gl.enable(gl.CULL_FACE);
-
         // which program gl should use
         gl.useProgram(program);
 
         // bind attribute/buffer
         gl.bindVertexArray(va);
 
-        // textura
-        gl.uniform1i(textureLocation, texture);
+        console.log(mode)
+
+        // texture
+        gl.uniform1i(textureLocation, 0); //texture
+        //mode
+        gl.uniform1i(programMode, mode);
+
+        //resolution
+        gl.uniformMatrix3fv(resolution, false, [
+            clipWidth, 0, 0,
+            0, clipHeight, 0,
+            clipX, clipY, 1,
+          ]);
 
         
         gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-        console.log('speed: ');
         requestAnimationFrame(draw);
     }    
 }
 
-//
-// Initialize a texture and load an image.
-// When the image finished loading copy it into the texture.
-//
-function loadTexture(gl, url) {
-    const tex = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, tex);
-  
-    // Because images have to be download over the internet
-    // they might take a moment until they are ready.
-    // Until then put a single pixel in the texture so we can
-    // use it immediately. When the image has finished downloading
-    // we'll update the texture with the contents of the image.
-    const level = 0;
-    const internalFormat = gl.RGBA;
-    const width = 1;
-    const height = 1;
-    const border = 0;
-    const srcFormat = gl.RGBA;
-    const srcType = gl.UNSIGNED_BYTE;
-    const pixel = new Uint8Array([0, 0, 1, 1]);  // opaque blue
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                  width, height, border, srcFormat, srcType,
-                  pixel);
-  
-    const image = new Image();
-    image.onload = function() {
-      gl.bindTexture(gl.TEXTURE_2D, tex);
-      gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                    srcFormat, srcType, image);
-  
-      // WebGL1 has different requirements for power of 2 images
-      // vs non power of 2 images so check if the image is a
-      // power of 2 in both dimensions.
-      if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-         // Yes, it's a power of 2. Generate mips.
-         gl.generateMipmap(gl.TEXTURE_2D);
-      } else {
-         // No, it's not a power of 2. Turn off mips and set
-         // wrapping to clamp to edge
-         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-      }
-    };
+function loadTexture(gl, image, url) {
+    const texture = gl.createTexture();
+    
     image.src = url;
+    image.onload = setTimeout(function() {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+        // Upload the image into the texture.
+        var mipLevel = 0;               // the largest mip
+        var internalFormat = gl.RGBA;   // format we want in the texture
+        var srcFormat = gl.RGBA;        // format of data we are supplying
+        var srcType = gl.UNSIGNED_BYTE; // type of data we are supplying
+        gl.texImage2D(gl.TEXTURE_2D,
+                        mipLevel,
+                        internalFormat,
+                        srcFormat,
+                        srcType,
+                        image);
+    }, 80);
   
-    return tex;
+    return image;
   }
+
   function isPowerOf2(value) {
     return (value & (value - 1)) == 0;
   }
 
 start();
-
